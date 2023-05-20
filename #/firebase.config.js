02a5d670de2c3ef05ac2419b45f9dbfbeb671064
firdaous <isiaqfridaus@gmail.com>
2023-05-20 14:04:08 -0700

    half-done file, to be finished by devcare and Mr Femi

--- #/firebase.config.js
+++ #/firebase.config.js
@@ -1,11 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDHi0WXilV2fRp3KRScOwKJS-Ln741nbxs",
   authDomain: "amamstore-b892b.firebaseapp.com",
@@ -47,17 +42,10 @@ function uploadimage() {
 
   // Get values
   var url = getInputVal("url");
-  // Save message
-  // saveMessage(url);
 }
 function getInputVal(id) {
   document.getElementById("contactForm").reset();
 }
-
-// // Function to get form values
-// function getInputVal(id) {
-//   return document.getElementById(id).value;
-// }
 
 // Save message to firebase database
 function saveMessage(url) {
--- #/js/scripts.js
+++ #/js/scripts.js
@@ -16,18 +16,6 @@ const images = document.querySelectorAll('.over-contain');
 const nextBtn = document.querySelector('next-click');
 let currentIndex = 0
 
-// function showImage(index){
-//     const imageWidth = images[0].clientWidth;
-//     imageContainer.style.transform = 'translateX(-${index * imageWidth}px)';
-// }
-
-// nextBtn.addEventListener('click', () =>) {
-//     if (currentIndex < images.length - 1){
-//         currentIndex++;
-        
-//     }
-// }
-
 const dropDown = document.querySelector('.btn-drop');
 const dropDown2 = document.querySelector('.btn-drop2');
 const dropDown3 = document.querySelector('.btn-drop3');
@@ -45,21 +33,3 @@ dropDown2.addEventListener('click',() => {
 dropDown3.addEventListener('click',() => {
     accDrop3.classList.toggle('active');
 });
-
-
-
-// JavaScript code on the home page
-// Retrieve item data from the Firebase database
-// firebase.database().ref("items").once("value").then(function(snapshot) {
-//     var items = snapshot.val();
-  
-//     // Handle the retrieved item data
-//     var itemsContainer = document.getElementById("itemsContainer");
-//     for (var itemId in items) {
-//       var item = items[itemId];
-//       var itemElement = document.createElement("div");
-//       itemElement.innerHTML = "<h3>" + item.itemName + "</h3><p>" + item.itemDescription + "</p><img src='" + item.downloadURL + "' alt='Item Image'>";
-//       itemsContainer.appendChild(itemElement);
-//     }
-//   });
-  
